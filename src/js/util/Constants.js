53c6b64da19bcf4f514a8d3e2e2ceec595376ffb
Alex Dumitru <alex@flanche.net>
2013-06-13 15:30:46 +0200

    constants added init

--- src/js/util/Constants.js
+++ src/js/util/Constants.js
@@ -1,16 +1,20 @@
-/* 
+/*
  * KAT Constants.
  * @author <a href="mailto:m.dumitru@jacobs-university.de">Alex Dumitru</a>
  * @author <a href="mailto:v.merticariu@jacobs-university,de">Vlad Merticariu</a>
  */
 
 FlancheJs.defineObject("kat.Constants", {
-    statics: {
-        TextPreprocessor:{
-            IdPrexif: "kat",
-            Selector: "body"
-        }
+  init   : function () {
+
+  },
+  
+  statics: {
+    TextPreprocessor: {
+      IdPrexif: "kat",
+      Selector: "body"
     }
+  }
 })
 
 
