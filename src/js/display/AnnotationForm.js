77d818b1535119279e1012e166a785c45b56e979
smirea <steven.mirea@gmail.com>
2014-02-15 01:41:50 -0800

    added GPL to all source code. Fixes #3

--- src/js/display/AnnotationForm.js
+++ src/js/display/AnnotationForm.js
@@ -1,3 +1,23 @@
+/*
+ * This file is part of KAT, the KWARC Annotation Tool, 
+ * see https://github.com/KWARC/KAT
+ * 
+ * Copyright (c) 2014 by the KWARC Group (http://kwarc.info)
+ * 
+ * KAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * KAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with KAT.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 /**
  * Class for handling the form displayed when an annotation is added.
  *
