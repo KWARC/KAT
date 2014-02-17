aaf393a3e982c039f00b451295e8799af9a1b6ab
smirea <steven.mirea@gmail.com>
2014-02-17 12:39:57 -0800

    new version of test

--- test/js/TextPreprocessor.js
+++ test/js/TextPreprocessor.js
@@ -1,6 +1,6 @@
-/* 
+/*
  * Test suite for TestPreprocessor.js
- * 
+ *
  * @author <a href="mailto:m.dumitru@jacobs-university.de">Alex Dumitru</a>
  * @author <a href="mailto:v.merticariu@jacobs-university,de">Vlad Merticariu</a>
  */
@@ -8,7 +8,7 @@
 $(document).ready(function(){
     textPreprocessor = new kat.TextPreprocessor("#text");
     textPreprocessor.run();
-    
+
     annotations = [
         {
             "idBase": "kat-140",
@@ -21,7 +21,7 @@ $(document).ready(function(){
             "content": "<b>Title:</b> Annotation 2 <br/> <b>Content:</b> This is the annotation content.<br/>"
         }
     ]
-    
+
     display = new kat.Display(annotations);
     display.run();
 })
--- test/js/run.js
+++ test/js/run.js
@@ -3,8 +3,49 @@
  * @author <a href="mailto:alex@flanche.net">Alex Dumitru</a>
  */
 jQuery(document).ready(function () {
+  var $loadFile = $(document.createElement('div'));
+
+  $loadFile.append(
+    $(document.createElement('input')).attr({
+      type: 'text',
+      placeholder: 'Paste an URL and press load',
+      value: 'demo-document-content.html',
+      style: 'width:400px; height:30px; line-height:20px; margin:0;',
+    }),
+    $(document.createElement('button')).
+      html('load').
+      addClass('btn').
+      on('click', function () {
+        $.get($loadFile.find('input[type="text"]').val(), initDemo);
+      })
+  );
+
+  $('#text').html($loadFile);
+});
+
+function initDemo (content) {
+  $('#text').html(content);
+  // For the demo only: load the localStorage from a file
+  if (localStorage.annotationRegistry) {
+    init();
+    return;
+  }
+
+  jQuery.getJSON('demo-localStorage.json', function (res) {
+    for (var key in res) {
+      localStorage[key] = res[key];
+    }
+    init();
+  }).fail(function () {
+    console.warn(arguments);
+  });
+}
+
+function init () {
   //nice and simple
   var service = new kat.main.KATService("#text");
   service.run();
-})
 
+  var reviewViewer = new kat.review.ReviewViewer(service);
+  reviewViewer.apply();
+}
