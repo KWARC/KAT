fdf05d816311aa61bc8d36747174f40d771f338f
Tom <tkw01536@gmail.com>
2014-04-12 09:05:15 +0200

    Updated ArrowConnector class

--- src/js/display/ArrowConnector.js
+++ src/js/display/ArrowConnector.js
@@ -18,62 +18,58 @@
  * along with KAT.  If not, see <http://www.gnu.org/licenses/>
  */
 
+
+
 /**
- * Creates an svg arrow that can be used to connect two dom elements, for example a
- * reference field annotation to the referenced item.
- *
- * @author <a href="mailto:m.dumitru@jacobs-university.de">Alex Dumitru</a>
- * @author <a href="mailto:v.merticariu@jacobs-university,de">Vlad Merticariu</a>
+ * Creates an svg arrow that can be used to connect two dom elements, for example a reference field annotation to the referenced item.
  */
+kat.display.ArrowConnector = function(arrowBaseElement, arrowHeadElement){
 
-FlancheJs.defineClass("kat.display.ArrowConnector", {
+    //store parameters
+    this._arrowBaseElement = arrowBaseElement;
+    this._arrowHeadElement = arrowHeadElement;
+    this_connection = null; 
 
-    init: function (arrowBaseElement, arrowHeadElement) {
-        this._arrowBaseElement = arrowBaseElement;
-        this._arrowHeadElement = arrowHeadElement;
-    },
+}
 
-    methods: {
-        render: function () {
-            if (!this._connection) {
-                this._createSVGArrow();
-            }
-        },
-
-        destroy: function () {
-            console.log("calldestroy"); 
-            if (this._connection) {
-                jsPlumb.detach(this._connection, {
-                    forceDetatch: true
-                })
-                this._connection = null;
-            }
-        }
-    },
-
-    internals: {
-        arrowBaseElement: null,
-        arrowHeadElement: null,
-        connection: null,
-
-        createSVGArrow: function () {
-            this._connection = jsPlumb.connect({
-                source: this._arrowBaseElement,
-                target: this._arrowHeadElement,
-                container: this._arrowBaseElement.parent(),
-                overlays: [
-                    "Arrow"
-                ],
-                paintStyle: {
-                    strokeStyle: "#5b9ada",
-                    lineWidth: 3
-                },
-                endpoint: "Blank",
-                connector: ["Bezier", {curviness: 70}]
-            });
-        }
-
+/*
+    Renders the arrow represented by this class instance. 
+*/
+kat.display.ArrowConnector.prototype.render = function () {
+    if (!this._connection) {
+        this._createSVGArrow();
     }
+}
 
+/*
+    Removes the rendered arrow represented by this class instance. 
+*/
+kat.display.ArrowConnector.prototype.destroy = function () {
+    // console.log("calldestroy"); 
+    if (this._connection) {
+        jsPlumb.detach(this._connection, {
+            forceDetatch: true
+        })
+        this._connection = null;
+    }
+}
 
-})
+/*
+    Creates the SVG object belonging to the arrow. 
+*/
+kat.display.ArrowConnector.prototype._createSVGArrow = function () {
+    this._connection = jsPlumb.connect({
+        source: this._arrowBaseElement,
+        target: this._arrowHeadElement,
+        container: this._arrowBaseElement.parent(),
+        overlays: [
+            "Arrow"
+        ],
+        paintStyle: {
+            strokeStyle: "#5b9ada",
+            lineWidth: 3
+        },
+        endpoint: "Blank",
+        connector: ["Bezier", {curviness: 70}]
+    });
+}; 
